e48e69501bb951a0562b674589de5981186ae8da
Nguyễn Đình Thái <dinhthai0312@gmail.com>
2025-01-02 22:57:08 +0700

    change host

--- simple_crm/settings.py
+++ simple_crm/settings.py
@@ -99,8 +99,7 @@ DATABASES = {
         'NAME': env('DATABASE_NAME'),
         'USER': env('DATABASE_USER'),
         'PASSWORD': env('DATABASE_PASSWORD'),
-        # 'HOST': env('DATABASE_HOST'),
-        'HOST': 'localhost',
+        'HOST': env('DATABASE_HOST'),
         'PORT': env('DATABASE_PORT'),
     }
 }
